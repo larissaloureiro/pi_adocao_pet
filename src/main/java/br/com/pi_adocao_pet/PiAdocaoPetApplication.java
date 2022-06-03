3ae0e98448b2e299c424947063e2fd265d688e01
Larissa Loureiro <larissa.loureiro@ymail.com>
2022-06-03 15:51:21 -0300

    Initial commit

---
+++ src/main/java/br/com/pi_adocao_pet/PiAdocaoPetApplication.java
@@ -0,0 +1,13 @@
+package br.com.pi_adocao_pet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiAdocaoPetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiAdocaoPetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/pi_adocao_pet/PiAdocaoPetApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pi_adocao_pet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiAdocaoPetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
